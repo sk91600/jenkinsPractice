6d09e2dd7f270c5033b77b98a0e6dfb442b91244
sk91600 <sk91600@163.com>
2019-08-16 15:52:03 +0800

    创建项目

---
+++ src/main/java/com/example/demo/controller/TestHello.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @program: jenkinsPractice
+ * @description: jenkins简单测试
+ * @author: Mr.Wang
+ * @create: 2019-08-16 15:31
+ **/
+@RestController
+public class TestHello {
+    @RequestMapping("/")
+    public String ShowMessage(){
+        return "Hello!!!";
+    }
+}

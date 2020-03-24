d671cdcfe6d9684ec790e3067603e02f69ab4691
songfangjia <sk91600@163.com>
2020-03-24 11:30:12 +0800

    修改测试代码

--- src/main/java/com/example/demo/controller/TestHello.java
+++ src/main/java/com/example/demo/controller/TestHello.java
@@ -14,6 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestHello {
     @RequestMapping("/")
     public String ShowMessage(){
-        return "Hello!!!123";
+        return "Hello!!!123456";
     }
 }
